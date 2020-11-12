485312c2c73a3b617663a7e3953a03cb821ade3d
Vamsi709 <70019341+Vamsi709@users.noreply.github.com>
2020-11-12 19:37:20 +0530

    Add files via upload

---
+++ GinGorm/createbook.go
@@ -0,0 +1,14 @@
+func CreateBook(c *gin.Context) {
+	// Validate input
+	var input CreateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+	  c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	  return
+	}
+  
+	// Create book
+	book := models.Book{Title: input.Title, Author: input.Author}
+	models.DB.Create(&book)
+  
+	c.JSON(http.StatusOK, gin.H{"data": book})
+  }
---
+++ GinGorm/updatebook.go
@@ -0,0 +1,19 @@
+func UpdateBook(c *gin.Context) {
+	// Get model if exist
+	var book models.Book
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+	  c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+	  return
+	}
+  
+	// Validate input
+	var input UpdateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+	  c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	  return
+	}
+  
+	models.DB.Model(&book).Updates(input)
+  
+	c.JSON(http.StatusOK, gin.H{"data": book})
+  }
